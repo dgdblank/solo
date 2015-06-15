67dea70a23c652c13b7bdfc5be58df1014366238
Danielle Blank <dgdblank@gmail.com>
2015-06-15 16:57:42 -0700

    Added angular table. Dynamically adding roommates to the table in progress.

--- client/app.js
+++ client/app.js
@@ -1,10 +1,27 @@
-var app = angular.module('app', [])
+angular.module('app', [
+	'ngTable'])
 
-app.controller('SplitCtrl', function ($scope) {
+.controller('SplitCtrl', function ($scope, ngTableParams) {
 	$scope.roommates = [];
 
 	$scope.addRoommate = function(person){
 		$scope.roommates.push(person);
 		$scope.newPerson = '';
 	}
+
+	$scope.tableParams = new ngTableParams({
+		page: 1,
+		count: 10
+	}, {
+		getData: function($defer, params) {
+			$defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+		}
+	})
+	
+
+	var data = [{name: "Moroni", age: 50},
+                {name: "Tiancum", age: 43},
+                {name: "Jacob", age: 27},
+                {name: "Nephi", age: 29},
+                {name: "Enos", age: 35}];
 });
